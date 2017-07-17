0f60b27104a92920d586c3b712e0b205919d73b9
Peter Johnson <insomnia@rcpt.at>
2017-07-17 19:43:23 +0200

    setup: add support for the --disable-data-download flag

--- attributes/defaults.rb
+++ attributes/defaults.rb
@@ -1,3 +1,4 @@
 default[:libpostal][:codedir] = "/usr/local/libpostal"
 default[:libpostal][:datadir] = "/usr/local/libpostal-data/"
 default[:libpostal][:revision] = 'master'
+default[:libpostal][:disable_data_download] = 'no'
